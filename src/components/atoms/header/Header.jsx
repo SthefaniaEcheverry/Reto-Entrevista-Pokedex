20f885642682edca57e7152f83da7633fd04604f
Sthefania Echeverry Orozco <sthefaniaecheverry@gmail.com>
2023-03-06 15:56:48 -0500

    delete files and create header

---
+++ src/components/atoms/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+function Header(){
+    return(
+        <div className='header'>
+            <img className='logo' src="/assets/pagesImages/logo.png" alt="logo" />
+            <h2 className='name-title'>POKEDEX</h2>
+        </div>
+    )
+}
+
+export default Header
